16c7b05318680244f253492809e5f15bd7d71235
RoberNET <robernet@gmail.com>
2015-09-27 09:23:08 -0500

    Update Menus

--- platforms/android/assets/www/js/mapas.js
+++ platforms/android/assets/www/js/mapas.js
@@ -19,25 +19,25 @@ var showMaps = function() {
 	mapa1.addMarker({
 		lat: 19.03051,
 		lng: -98.27785,
-		title: 'Matriz Puebla',
+		title: 'Puebla Matriz',
 		infoWindow: {
-			content: '<p>Matriz Puebla<br /><br /><a href="geo:0,0?q=19.03051,-98.27785(Pronta%20Tres%20Puebla)">Abrir en Google Maps</p></a>'
+			content: '<p>Puebla Matriz<br /><br /><a href="geo:0,0?q=19.03051,-98.27785(Pronta%20Tres%20Puebla%20Matriz)">Abrir en Google Maps</p></a>'
 		}
 	});
 	mapa2.addMarker({
 		lat: 20.99716,
 		lng: -89.63080,
-		title: 'Merida 1',
+		title: 'Merida Sucursal Circuito',
 		infoWindow: {
-			content: '<p>Merida 1<br /><br /><a href="geo:0,0?q=20.99716,-89.63080(Pronta%20Tres%20Merida%201)">Abrir en Google Maps</p></a>'
+			content: '<p>Merida Sucursal Circuito<br /><br /><a href="geo:0,0?q=20.99716,-89.63080(Pronta%20Tres%20Merida%20Circuito)">Abrir en Google Maps</p></a>'
 		}
 	});
 	mapa3.addMarker({
 		lat: 21.00614,
 		lng: -89.62285,
-		title: 'Merida 2',
+		title: 'Merida Sucursal 60 Norte',
 		infoWindow: {
-			content: '<p>Merida 2<br /><br /><a href="geo:0,0?q=21.00614,-89.62285(Pronta%20Tres%20Merida%202)">Abrir en Google Maps</p></a>'
+			content: '<p>Merida Sucursal 60 Norte<br /><br /><a href="geo:0,0?q=21.00614,-89.62285(Pronta%20Tres%20Merida%2060%20Norte)">Abrir en Google Maps</p></a>'
 		}
 	});
 }
